57370f2f8277f982cce9ee17515edd7c178590b6
runner989 <runner989@hotmail.com>
2025-08-13 18:51:51 -0400

    fixed broken code for testing

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,7 @@
 import { IncomingHttpHeaders } from "http";
 
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
-  const authHeader = headers["authorizations"];
+  const authHeader = headers["authorization"];
   if (!authHeader) {
     return null;
   }
